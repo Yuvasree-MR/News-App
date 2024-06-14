c3027bf4a083d9b9a00ebc82d2c99fa3b6301b53
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-14 13:55:55 +0530

    Adjusted Batch position

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:1}}>{source} </span>
+          <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0'}}>
+            <span className="badge rounded-pill bg-danger">{source} </span>
+          </div>
+        
           <img src={!imageUrl?"https://thumbs.dreamstime.com/b/news-woodn-dice-depicting-letters-bundle-small-newspapers-leaning-left-dice-34802664.jpg":imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
